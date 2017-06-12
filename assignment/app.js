932dc9af3a898f87a45031ee25eaf4af199ed103
soumyamathew09 <soumimathew@gmail.com>
2017-06-12 13:35:22 -0400

    added mongo dependencies

--- assignment/app.js
+++ assignment/app.js
@@ -1,3 +1,18 @@
+var mongoose = require('mongoose');
+
+var connectionString = 'mongodb://127.0.0.1:27017/assignment'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds137281.mlab.com:37281/heroku_0z73g7zn'; // user yours
+}
+
+mongoose.connect(connectionString);
+
+//mongoose.connect('mongodb://localhost/assignment');
+mongoose.Promise = require('q').Promise;
+
 require('./services/user.service.server');
 require('./services/website.service.server');
 require('./services/page.service.server');
