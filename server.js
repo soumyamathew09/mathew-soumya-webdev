8839fa758248fc44035a615c15503a04e9e253e6
soumyamathew09 <soumimathew@gmail.com>
2017-06-16 13:11:33 -0400

    dependencies added - environment variables

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var app = require('./express');
 var bodyParser = require('body-parser');
+var passport      = require('passport');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({ secret: process.env.SESSION_SECRET}))    ;
+
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // configure a public directory to host static content
 app.use(app.express.static(__dirname + '/public'));
