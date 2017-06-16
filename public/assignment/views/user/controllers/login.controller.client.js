0f2e627e604ece22522bcbfbcacb604dbf9ba865
soumyamathew09 <soumimathew@gmail.com>
2017-06-16 12:53:07 -0400

    login - session user

--- public/assignment/views/user/controllers/login.controller.client.js
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -7,22 +7,45 @@
 
         var model = this;
         model.login = login;
+        model.logout = logout;
 
         function login (username,password) {
-            UserService
-                .findUserByCredentials(username,password)
-                .then(function (found) {
-                    if(found !== null){
-                        $location.url('/user/' + found._id)
-                    }else {
-                        model.message = "Incorrect username or password. Please try again.";
-                    }
-                },
-                    function () {
-                        model.message = "Incorrect username or password. Please try again.";
-                    }
-                );
 
+           if (typeof username === "undefined" || typeof password === "undefined"){
+               model.message = "Please enter username and password";
+           }
+           else {
+
+               var user = {
+                   username: username,
+                   password: password
+               }
+
+               UserService
+                   .login(user)
+                   .then(function (found) {
+                           if (found !== null) {
+                               var user = found;
+                               $location.url("/profile");
+                           } else {
+                               model.message = "Incorrect username or password. Please try again.";
+                           }
+                       },
+                       function () {
+                           model.message = "Incorrect username or password. Please try again.";
+                       }
+                   );
+           }
         }
+
+        function logout() {
+            UserService
+                .logout()
+                .then(
+                    function(response) {
+                        $location.url("/");
+                    });
+        }
+
     }
 })();
